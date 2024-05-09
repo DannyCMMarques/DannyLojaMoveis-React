4b140f7a54fb6db04e9a968c1b5d0f544883c4a4
Danny Marques <daninhacmmarques@gmail.com>
2024-05-08 21:43:52 -0300

    [UPDATE] Correção responsive

--- components/Painel/Painel.jsx
+++ components/Painel/Painel.jsx
@@ -1,37 +1,34 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import styles from './Painel.module.css'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import styles from "./Painel.module.css";
 const Painel = () => {
   return (
     <div className={styles.painel}>
-      <Container fluid >
+      <Container fluid>
         <Row>
           <Col md={7} sm={5}>
             <div className={styles.containerInformacao}>
-            <div>
-              <p className={styles.slogan}>
-                Transforme sua casa em um lar com conforto
-              </p>
-            </div>
-            <div>
-              <p className={styles.slogan2}>
-                Descubra a sua morada dos sonhos conosco
-              </p>
-            </div>
+              <div>
+                <p className={styles.slogan}>
+                  Transforme sua casa em um lar com conforto
+                </p>
+              </div>
+              <div>
+                <p className={styles.slogan2}>
+                  Descubra a sua morada dos sonhos conosco
+                </p>
+              </div>
 
-            <Button className={styles.orangeButton}> COMPRE AGORA </Button>
+              <Button className={styles.orangeButton}> COMPRE AGORA </Button>
             </div>
           </Col>
           <Col md="5"></Col>
-
-
         </Row>
       </Container>
-
     </div>
   );
-}
+};
 
 export default Painel;
